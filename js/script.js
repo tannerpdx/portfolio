a2d30696c1f157a17ed439d03addba75950884d7
Tanner Cheek <236692562+tannerpdx@users.noreply.github.com>
2025-12-02 22:20:11 -0800

    added hero section, fixed nav mobile issues

--- js/script.js
+++ js/script.js
@@ -1,2 +1,8 @@
 // js loaded confirmation
 console.log("portfolio loaded!");
+
+// get the current year
+const yearEl = document.getElementById("year");
+if (yearEl) {
+  yearEl.textContent = new Date().getFullYear();
+}
